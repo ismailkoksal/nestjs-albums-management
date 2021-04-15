9d181370ab1da845996a6ac4951bb2e5f4a0bd62
Ismail Koksal <ismail@koksal.fr>
2021-04-15 13:50:14 +0200

    Create albums

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AlbumsModule],
   controllers: [],
   providers: [],
 })
