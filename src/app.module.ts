ebe66b0c8eb7589e225125cf9c69de5bc2d96e73
Ismail Koksal <ismail@koksal.fr>
2021-04-15 13:50:50 +0200

    Create artists

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AlbumsModule } from './albums/albums.module';
+import { ArtistsModule } from './artists/artists.module';
 
 @Module({
-  imports: [UsersModule, AlbumsModule],
+  imports: [UsersModule, AlbumsModule, ArtistsModule],
   controllers: [],
   providers: [],
 })
