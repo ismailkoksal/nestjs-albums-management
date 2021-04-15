9e242941d7cf40afa11a176f6b974cdec92f7194
Ismail Koksal <ismail@koksal.fr>
2021-04-15 14:10:23 +0200

    Create song entity

--- src/songs/entities/song.entity.ts
+++ src/songs/entities/song.entity.ts
@@ -1,1 +1,17 @@
-export class Song {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Album } from '../../albums/entities/album.entity';
+
+@Entity()
+export class Song {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  duration: string;
+
+  @ManyToOne(() => Album, (album) => album.songs)
+  album: Album;
+}
