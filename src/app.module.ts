9429da449e067718ab6d788893f9ce8f8e396eba
Ismail Koksal <ismail@koksal.fr>
2021-04-15 13:51:23 +0200

    Create songs

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
+import { SongsModule } from './songs/songs.module';
 
 @Module({
-  imports: [UsersModule, AlbumsModule, ArtistsModule],
+  imports: [UsersModule, AlbumsModule, ArtistsModule, SongsModule],
   controllers: [],
   providers: [],
 })
