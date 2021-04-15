9afb00bab417fa63c245d7e916dfb446cd78df64
Ismail Koksal <ismail@koksal.fr>
2021-04-15 14:12:30 +0200

    Create artist entity

--- src/artists/entities/artist.entity.ts
+++ src/artists/entities/artist.entity.ts
@@ -1,1 +1,17 @@
-export class Artist {}
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import { Album } from '../../albums/entities/album.entity';
+
+@Entity()
+export class Artist {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  isBand: boolean;
+
+  @OneToMany(() => Album, (album) => album.artist)
+  albums: Album[];
+}
