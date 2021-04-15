b58789f2b0d0daa0136f90be0dbd9451a0f18ddb
Ismail Koksal <ismail@koksal.fr>
2021-04-15 14:07:58 +0200

    Create album entity

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,1 +1,30 @@
-export class Album {}
+import {
+  Column,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Song } from '../../songs/entities/song.entity';
+import { Artist } from '../../artists/entities/artist.entity';
+
+@Entity()
+export class Album {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column('year')
+  year: number;
+
+  @Column()
+  cover: string;
+
+  @ManyToOne(() => Artist, (artist) => artist.albums)
+  artist: Artist;
+
+  @OneToMany(() => Song, (song) => song.album)
+  songs: Song[];
+}
